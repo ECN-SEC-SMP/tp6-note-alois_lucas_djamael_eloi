766f1032e072834803e2f09a26d5e4271a312e80
Eloi Tourangin <Eloi.Tourangin@eleves.ec-nantes.fr>
2026-01-20 15:33:40 +0100

    add Plateau class

---
+++ include/plateau.hpp
@@ -0,0 +1,42 @@
+/**
+ * @file plateau.hpp
+ * @author Eloi Tourangin (Eloi.Tourangin@eleves.ec-nantes.fr)
+ * @brief
+ * @version 0.1
+ * @date 20-01-2026
+ *
+ * @copyright Copyright (c) 2026
+ *
+ */
+
+#pragma once
+
+#include <vector>
+#include "Case.hpp"
+#include "Pion.hpp"
+
+using namespace std;
+
+class Plateau
+{
+private:
+    vector<vector<Case>> grille;
+
+    bool verifierLignes();
+
+    bool verifierColonnes();
+
+    bool verifierDiagonales();
+    
+    bool verifierEmpilements();
+
+public:
+    Plateau();
+    ~Plateau() = default;
+
+    bool placerPion(int x, int y, Pion *p);
+
+    Case *getCase(int x, int y);
+
+    bool verifierVictoire();
+};
---
+++ src/plateau.cpp
@@ -0,0 +1,30 @@
+/**
+ * @file plateau.cpp
+ * @author Eloi Tourangin (Eloi.Tourangin@eleves.ec-nantes.fr)
+ * @brief 
+ * @version 0.1
+ * @date 20-01-2026
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
+
+#include "plateau.hpp"
+
+Plateau::Plateau()
+{
+}
+
+bool Plateau::placerPion(int x, int y, Pion *p)
+{
+}
+
+Case *Plateau::getCase(int x, int y)
+{
+    return &grille[y][x];
+}
+
+bool Plateau::verifierVictoire()
+{
+    return verifierLignes() || verifierColonnes() || verifierDiagonales() || verifierEmpilements();
+}
