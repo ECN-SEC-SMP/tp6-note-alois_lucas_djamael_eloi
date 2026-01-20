7d21c59b32deb896fce04f01d67b08e4b33080bc
Aloïs Guillon <alois.guillon@gmail.com>
2026-01-20 15:58:40 +0100

    Update pion.cpp et pion.hpp

--- include/pion.hpp
+++ include/pion.hpp
@@ -1,10 +1,41 @@
 /**
  * @file pion.hpp
  * @author Aloïs GUILLON (alois.guillon@eleves.ec-nantes.fr)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2026-01-20
- * 
+ *
  * @copyright Copyright (c) 2026
- * 
+ *
  */
+
+#pragma once
+
+enum Couleur
+{
+    ROUGE,
+    VERT,
+    BLEU,
+    JAUNE
+};
+
+enum Taille
+{
+    PETIT,
+    MOYEN,
+    GRAND
+};
+
+class Pion
+{
+private:
+    Couleur couleur;
+    Taille taille;
+
+public:
+    Pion(Couleur, Taille);
+
+    Couleur getCouleur();
+
+    Taille getTaille();
+};
--- src/pion.cpp
+++ src/pion.cpp
@@ -1,10 +1,28 @@
 /**
  * @file pion.cpp
  * @author Aloïs GUILLON (alois.guillon@eleves.ec-nantes.fr)
- * @brief 
+ * @brief
  * @version 0.1
  * @date 2026-01-20
- * 
+ *
  * @copyright Copyright (c) 2026
- * 
+ *
  */
+
+#include "pion.hpp"
+
+Pion::Pion(Couleur c, Taille t)
+{
+    this->couleur = c;
+    this->taille = t;
+}
+
+Couleur Pion::getCouleur()
+{
+    return this->couleur;
+}
+
+Taille Pion::getTaille()
+{
+    return this->taille;
+}
