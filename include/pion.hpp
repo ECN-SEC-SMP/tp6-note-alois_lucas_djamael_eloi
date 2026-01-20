373e65d90f571e17a323e834e5137eb9132caae6
Aloïs Guillon <alois.guillon@gmail.com>
2026-01-20 15:21:11 +0100

    Ajout pion.cpp et pion.hpp

---
+++ include/pion.hpp
@@ -0,0 +1,10 @@
+/**
+ * @file pion.hpp
+ * @author Aloïs GUILLON (alois.guillon@eleves.ec-nantes.fr)
+ * @brief 
+ * @version 0.1
+ * @date 2026-01-20
+ * 
+ * @copyright Copyright (c) 2026
+ * 
+ */
